c262db837b7c072cebde530fedaa1adf9b2243c7
neetasawant <neeta.sawant0395@gmail.com>
2024-10-01 01:31:10 +0530

    manifest file updated

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-
+import '@/assets/styles.css'
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className='bg-red-500'>
+      <h1 className='text-black'>AI Prompt</h1>
+    </div>
   );
 }
 
